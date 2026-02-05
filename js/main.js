e096db748ba612efa3f0da3a30bf4a63c57e0d6a
Alam Rosario <alamsanchez05@gmail.com>
2026-02-04 20:00:25 -0400

    Corrección de estilos y menú móvil

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
+const navLinks = document.getElementById("navLinks"); // Corregido
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
